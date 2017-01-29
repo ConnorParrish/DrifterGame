a1ba7e1715f2a828343971ed406b742a2bb7abb7
Heimdyll <real.heimdyll@gmail.com>
2017-01-29 08:43:42 -0700

    Commented code for inventory backbone

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -12,24 +12,26 @@ public class PlayerInventory : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		itemDatabase = JsonMapper.ToObject(File.ReadAllText("ItemList.json"));
-		inventoryDatabase = JsonMapper.ToObject(File.ReadAllText("InventoryList.json"));
-		ConstructItemDatabase();
+		itemDatabase = JsonMapper.ToObject(File.ReadAllText("ItemList.json"));	// This is all the possible items a player could have
+		inventoryDatabase = JsonMapper.ToObject(File.ReadAllText("InventoryList.json"));	// This is all the player's current inventory (Used in saving)
+		ConstructItemDatabase();	// Builds the inventory list from the database
 
 	}
 
 	void ConstructItemDatabase(){
 		for (int i = 0; i < inventoryDatabase.Count; i++){
-			inventory.Add(new Item((int)inventoryDatabase[i]["id"], inventoryDatabase[i]["title"].ToString(), inventoryDatabase[i]["description"].ToString()));
+			inventory.Add(new Item((int)inventoryDatabase[i]["id"], inventoryDatabase[i]["title"].ToString(), 
+				inventoryDatabase[i]["description"].ToString(), (bool)inventoryDatabase[i]["stackable"]));
 		}
 	}
 
 	public void AddItem(int itemID) {
-		//Item newItem = new Item(id, title, description);
-		inventory.Add(new Item((int)itemDatabase[itemID]["id"], itemDatabase[itemID]["title"].ToString(), itemDatabase[itemID]["description"].ToString()));
+		inventory.Add(new Item((int)itemDatabase[itemID]["id"], itemDatabase[itemID]["title"].ToString(), 
+			itemDatabase[itemID]["description"].ToString(), (bool)itemDatabase[itemID]["stackable"]));
 		DebugInventory();
 	}
 
+	// Used to make sure inventory behaviour is working as intended
 	public void DebugInventory(){
 		for (int i = 0; i < inventory.Count; i++){
 			Debug.Log(inventory[i].Title);
@@ -40,12 +42,12 @@ public class PlayerInventory : MonoBehaviour {
 }
 
 public class Item {
-	// Update is called once per frame
 	public int ID { get; set; }
 	public string Title { get; set; }
 	public string Description { get; set; }
 
-	public Item(int id, string title, string description) {
+	// This is the constructor. You should already know that, though.
+	public Item(int id, string title, string description, bool stackable) {
 		this.ID = id;
 		this.Title = title;
 		this.Description = description;
