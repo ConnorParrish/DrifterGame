94fa85af8d7843c40937d775f40ec6ef6ac6c389
Heimdyll <heimdyll@outlook.com>
2017-01-29 17:06:48 -0700

    Added a rotating item script for the inventory that is not yet implemented. Fixed render settings for Inventory menu.

---
+++ Assets/RotatingItem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatingItem : MonoBehaviour {
+
+	public float yRotation;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		gameObject.transform.Rotate(0,yRotation,0);
+	}
+}
