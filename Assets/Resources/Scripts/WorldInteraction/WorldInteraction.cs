4aa9ca4ab1ee445541361366002b5d6c9a3f1272
gpsgamer <sutyak.geoffrey@gmail.com>
2017-02-14 22:22:16 -0700

    Reorganized the Models so they are all in one place.

--- Assets/Resources/Scripts/WorldInteraction/WorldInteraction.cs
+++ Assets/Resources/Scripts/WorldInteraction/WorldInteraction.cs
@@ -9,14 +9,16 @@ public class WorldInteraction : MonoBehaviour {
 	private bool walking;
 	private bool clickedPanhandle;
 	private GameObject destinationObject;
-	
+    private GameObject player;
 
 	// Use this for initialization
 	void Start () {
 		destinationObject = new GameObject();
 		destinationObject.AddComponent<SpriteRenderer>();
 		destinationObject.GetComponent<SpriteRenderer>().sprite = destinationSprite;
-				
+        player = GameObject.FindWithTag("Player");
+
+
 		navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
 	}
 	
@@ -34,6 +36,7 @@ public class WorldInteraction : MonoBehaviour {
 				} else {
 					navMeshAgent.stoppingDistance = 0f;
 					walking=true;
+                    player.GetComponentInChildren<Animator>().SetBool("IsWalking", walking);
 					destinationObject.SetActive(true);
 					destinationObject.transform.position = hit.point;
 					navMeshAgent.destination = hit.point;
@@ -46,7 +49,8 @@ public class WorldInteraction : MonoBehaviour {
 		if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance){
 			if (!navMeshAgent.hasPath || Mathf.Abs (navMeshAgent.velocity.sqrMagnitude) < float.Epsilon){
 				walking = false;
-				destinationObject.SetActive(false);
+                player.GetComponentInChildren<Animator>().SetBool("IsWalking", walking);
+                destinationObject.SetActive(false);
 			} else {
 				walking = true;
 				
