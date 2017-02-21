ee180db90557b75fc7530f19ca990e3e1ed2935f
Heimdyll <heimdyll@outlook.com>
2017-02-20 22:49:42 -0700

    I needed a single "return;" in the WorldInteraction.cs to fix the animation not always firing.

--- Assets/Resources/Scripts/WorldInteraction/WorldInteraction.cs
+++ Assets/Resources/Scripts/WorldInteraction/WorldInteraction.cs
@@ -6,7 +6,7 @@ public class WorldInteraction : MonoBehaviour {
 	public Sprite destinationSprite;
     public bool canMove = true;
 	private UnityEngine.AI.NavMeshAgent navMeshAgent;
-	private bool walking;
+	public bool walking;
 	private bool clickedPanhandle;
 	private GameObject destinationObject;
     private Animator anim;
@@ -27,36 +27,44 @@ public class WorldInteraction : MonoBehaviour {
 
         if (Input.GetButtonDown("Fire2"))
         {
+            //Debug.Break();
             Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 		    RaycastHit[] hits = Physics.RaycastAll(ray, 100);
 
 			if (hits != null && canMove){
                 foreach (RaycastHit hit in hits)
                 {
+                    Debug.Log("Collider name" + hit.collider.gameObject.name);
                     if (hit.collider.gameObject.tag == "Interactable Object")
                     {
+                        Debug.Log("Is Interacting...");
                         hit.collider.gameObject.GetComponent<Interactable>().MoveToInteraction(navMeshAgent);
                     }
                     else if (hit.collider.gameObject.tag == "Walkable")
                     {
+                        Debug.Log("isWalking");
                         navMeshAgent.stoppingDistance = 0f;
                         walking = true;
-                        anim.SetBool("IsWalking", walking);
+                        Debug.Log("walking: " + walking);
+                        anim.SetBool("IsWalking", true);
                         destinationObject.SetActive(true);
                         destinationObject.transform.position = hit.point;
                         navMeshAgent.destination = hit.point;
                         navMeshAgent.Resume();
+                        
 
                     }
 
                 }
             }
+            return;
 		}
 
 		if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance){
 			if (!navMeshAgent.hasPath || Mathf.Abs (navMeshAgent.velocity.sqrMagnitude) < float.Epsilon){
+                Debug.Log("if this shows up... hmmm");
 				walking = false;
-                anim.SetBool("IsWalking", walking);
+                anim.SetBool("IsWalking", false);
                 destinationObject.SetActive(false);
 			} else {
 				walking = true;
