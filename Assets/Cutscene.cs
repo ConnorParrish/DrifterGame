a38a5c4500d8e49314c79d6dd25b1109551bc104
gpsgamer <sutyak.geoffrey@gmail.com>
2017-04-05 18:09:43 -0600

    Cutscene is implemented. Badly.

---
+++ Assets/Cutscene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cutscene : MonoBehaviour {
+
+
+    public GameObject playerPrefab;
+    public GameObject maincamera;
+    public GameObject Buscamera; 
+
+    public void DropOffPlayer()
+    {
+        Instantiate(playerPrefab, transform);
+        Buscamera.SetActive(false);
+        maincamera.SetActive(true);
+
+    }
+
+
+    public void EndOfCutscene()
+
+    {
+        Destroy(gameObject);
+        
+    }
+
+}
