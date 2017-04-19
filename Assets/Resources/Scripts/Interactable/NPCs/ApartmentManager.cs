3e8f1be962b603a53b58f2325cf8c0e7a70afe69
Skyler Hektner <skylerhektner@gmail.com>
2017-04-19 01:53:53 -0600

    Implement Apartment Manager and Motel Keeper to an acceptable level for tomorrow. Will need to finish later.

---
+++ Assets/Resources/Scripts/Interactable/NPCs/ApartmentManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApartmentManager : NPCInteraction {
+
+    public override void Start()
+    {
+        base.NPCData = GameObject.Find("NPC Manager").GetComponent<NPCDatabase>().npcDict["Apartment Manager"];
+        base.Start();
+    }
+}
