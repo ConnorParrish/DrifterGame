c6363e5af2f82b8b47e04ecc9d1250c4e0d316ee
Skyler Hektner <skylerhektner@gmail.com>
2017-04-22 18:58:23 -0600

    Linked play game and quit buttons in opening menu. They now both work.

---
+++ Assets/Resources/Scripts/UI/LoadScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadScene : MonoBehaviour
+{
+    public string sceneName;
+
+    // Use this for initialization
+    void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(Load);
+    }
+
+    void Load()
+    {
+        SceneManager.LoadSceneAsync(sceneName);
+    }
+}
---
+++ Assets/Resources/Scripts/UI/QuitOnClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitOnClick : MonoBehaviour {
+
+    void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(Quit);
+    }
+
+    void Quit()
+    {
+        Debug.Log("TRYING TO QUIT");
+        Application.Quit();
+    }
+}
